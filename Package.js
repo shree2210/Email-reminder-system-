0b3e2d7e1f8828155b47c363c6dff19397e9cd69
shree2210 <rajasriprinces@gmail.com>
2025-10-31 13:26:22 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,16 @@
+{
+  “name”: “email-reminder-system”,
+  “version”: “1.0.0”,
+  “description”: “A simple Email Reminder System using Node.js, Express, and Nodemailer”,
+  “main”: “server.js”,
+  “scripts”: {
+    “start”: “node server.js”
+  },
+  “dependencies”: {
+    “express”: “^4.19.2”,
+    “mongoose”: “^8.5.0”,
+    “nodemailer”: “^6.9.8”,
+    “node-cron”: “^3.0.3”,
+    “dotenv”: “^16.4.5”
+  }
+} 
